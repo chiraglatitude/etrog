16e399751142143d07f274ccfee6dd04ef1b3302
chiraglatitude <chirag.solanki@latitudetechnolabs.com>
2021-11-30 15:50:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+/* Shapes */
+var animItem = bodymovin.loadAnimation({
+  container: document.getElementById("svgContainer"),
+  //   wrapper: svgContainer,
+  animType: "svg",
+  loop: true,
+  path: "data.json",
+});
